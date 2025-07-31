56b60ceaff0e2caf855f6088d64bff47a094b285
lbogerd <l.bogerd@protonmail.com>
2025-07-31 23:55:48 +0200

    feat: failed attempt at reducing gliding

--- src/RobotVacuum.tsx
+++ src/RobotVacuum.tsx
@@ -1,9 +1,11 @@
 import { useFrame } from "@react-three/fiber";
-import { RapierRigidBody, RigidBody } from "@react-three/rapier";
-import { useEffect, useRef } from "react";
+import { RapierRigidBody, RigidBody, vec3 } from "@react-three/rapier";
+import { useEffect, useRef, useState } from "react";
 
 export function RobotVacuum() {
-  const ref = useRef<null | RapierRigidBody>(null);
+  const robotBody = useRef<null | RapierRigidBody>(null);
+  const [logCounter, setLogCounter] = useState(0);
+  const [frameCounter, setFrameCounter] = useState(0);
 
   // Keyboard controls
   const keys = useRef<{ [key: string]: boolean }>({});
@@ -26,26 +28,56 @@ export function RobotVacuum() {
 
   // Physics step
   useFrame(() => {
-    if (!ref.current) return;
+    setLogCounter(logCounter + 1);
+    setFrameCounter(frameCounter + 1);
 
-    const force = 0.1;
-    const impulse = {
-      x: 0,
-      y: 0,
-      z: 0,
-    };
+    if (!robotBody.current) return;
 
-    if (keys.current["w"] || keys.current["ArrowUp"]) impulse.z -= force;
-    if (keys.current["s"] || keys.current["ArrowDown"]) impulse.z += force;
-    if (keys.current["a"] || keys.current["ArrowLeft"]) impulse.x -= force;
-    if (keys.current["d"] || keys.current["ArrowRight"]) impulse.x += force;
+    // log every 30 frames
+    if (logCounter >= 29) {
+      console.log(`CURRENT VELOCITIES | frame: ${frameCounter}`);
+      console.log(
+        `linear velocity: ${JSON.stringify(robotBody.current.linvel())}`
+      );
+      console.log(
+        `angular velocity: ${JSON.stringify(robotBody.current.angvel())}`
+      );
 
-    ref.current.addForce(impulse, true);
+      setLogCounter(0);
+    }
+
+    const keysPressed = Object.values(keys.current).length !== 0;
+
+    if (keysPressed) {
+      const force = 0.1;
+      const impulse = {
+        x: 0,
+        y: 0,
+        z: 0,
+      };
+
+      if (keys.current["w"] || keys.current["ArrowUp"]) impulse.z -= force;
+      if (keys.current["s"] || keys.current["ArrowDown"]) impulse.z += force;
+      if (keys.current["a"] || keys.current["ArrowLeft"]) impulse.x -= force;
+      if (keys.current["d"] || keys.current["ArrowRight"]) impulse.x += force;
+
+      robotBody.current.addForce(impulse, true);
+    } else {
+      const linearVector = vec3(robotBody.current.linvel());
+      const angularVector = vec3(robotBody.current.linvel());
+
+      if (linearVector.length() < 100) {
+        robotBody.current.setLinvel({ x: 0, y: 0, z: 0 }, true);
+      }
+      if (angularVector.length() < 100) {
+        robotBody.current.setAngvel({ x: 0, y: 0, z: 0 }, true);
+      }
+    }
   });
 
   return (
     <RigidBody
-      ref={ref}
+      ref={robotBody}
       mass={1}
       position={[0, 0.1, 0]}
       restitution={8} // Bounciness
